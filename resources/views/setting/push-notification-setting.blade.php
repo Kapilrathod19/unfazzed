dbabef10d5100cc8ddafc581e98990bbac668510
Kapilrathod19 <kapilrathod3456@gmail.com>
2026-05-31 14:09:47 +0530

    notification comment image

--- resources/views/setting/push-notification-setting.blade.php
+++ resources/views/setting/push-notification-setting.blade.php
@@ -57,7 +57,7 @@
                                 {{ html()->label(trans('messages.description') . ' <span class="text-danger">*</span>', 'description')->class('form-control-label') }}
                                 {{ html()->textarea('description', null)->class('form-control textarea')->id('description')->rows(3)->required()->placeholder(__('messages.description')) }}
                             </div>
-                            <div class="form-group col-md-12">
+                            {{-- <div class="form-group col-md-12">
                                 {{ html()->label(trans('messages.image'), 'notification_image')->class('form-control-label') }}
                                 <div class="custom-file">
                                     {{ html()->file('notification_image')->class('custom-file-input custom-file-input-sm detail')->id('notification_image')->attribute('accept', 'image/*') }}
@@ -65,7 +65,7 @@
                                 </div>
                                 <small class="help-block with-errors text-danger"></small>
                                 <p class="text-muted mt-1">{{ __('messages.default_notification_logo_hint', ['default' => __('messages.website_logo')]) ?? 'Note: If no image is uploaded, the website logo will be used.' }}</p>
-                            </div>
+                            </div> --}}
                         </div>
                         
                         {{ html()->submit(trans('messages.save'))->class('btn btn-md btn-primary float-end') }}
